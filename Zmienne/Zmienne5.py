e69225fb2c8044a236097a9184815312e993c2ef
Hypytysz <arek1nowosielski@gmail.com>
2022-06-08 14:06:23 +0200

    Przeniesienie do folderu Operatory

--- Zmienne/Zmienne5.py
+++
@@ -1,6 +0,0 @@
-#Pole trójkąta
-pole_podstawy = float(input("Podaj pole podstawy:"))
-wysokosc = float(input("Podaj wysokosc trojkata:"))
-
-pole = float ((pole_podstawy*wysokosc)/2)
-print(f"Pole trojkata wynosi: {pole}")
