dda92ebcbdcccca146c5a8afd1912578b09f666e
Hypytysz <arek1nowosielski@gmail.com>
2022-06-08 13:29:48 +0200

    Pole trójkąta

---
+++ Zmienne/Zmienne5.py
@@ -0,0 +1,6 @@
+#Pole trójkąta
+pole_podstawy = float(input("Podaj pole podstawy:"))
+wysokosc = float(input("Podaj wysokosc trojkata:"))
+
+pole = float ((pole_podstawy*wysokosc)/2)
+print(f"Pole trojkata wynosi: {pole}")
