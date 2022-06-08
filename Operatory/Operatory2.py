d17c8652f15af7ee44289029b4a3b0322a363e71
Hypytysz <arek1nowosielski@gmail.com>
2022-06-08 14:02:05 +0200

    Srednia pieciu liczb podanych przez uzytkownika

---
+++ Operatory/Operatory2.py
@@ -0,0 +1,9 @@
+#Średnia
+liczba1 = float(input("Podaj pierwsza liczbe:"))
+liczba2 = float(input("Podaj druga liczbe:"))
+liczba3 = float(input("Podaj trzecia liczbe:"))
+liczba4 = float(input("Podaj czwarta liczbe:"))
+liczba5 = float(input("Podaj piata liczbe:"))
+
+srednia = float ((liczba1+liczba2+liczba3+liczba4+liczba5)/5)
+print(f"srednia wynosi: {srednia}")
