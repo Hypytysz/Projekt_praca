255537ee5c37e60ccd116eb3c4cf095d229be51a
Hypytysz <107093668+Hypytysz@users.noreply.github.com>
2022-06-08 13:33:49 +0200

    Średnia pięciu liczb podanych przez użytkownika

--- Zmienne/Zmienne4.py
+++ Zmienne/Zmienne4.py
@@ -1,11 +1,9 @@
-#Srednia pieciu liczb podanych przez uzytkownika
+#Srednia
 liczba1 = float(input("Podaj pierwsza liczbe:"))
 liczba2 = float(input("Podaj druga liczbe:"))
 liczba3 = float(input("Podaj trzecia liczbe:"))
 liczba4 = float(input("Podaj czwarta liczbe:"))
 liczba5 = float(input("Podaj piata liczbe:"))
-ilosc = int(input("Ile liczb podales?:"))
 
-
-srednia = float ((liczba1+liczba2+liczba3+liczba4)/ilosc)
+srednia = float ((liczba1+liczba2+liczba3+liczba4+liczba5)/5)
 print(f"srednia wynosi: {srednia}")
