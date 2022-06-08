9c3cf2a98e2ebb3e3f217556c24eca630ffb19a7
Hypytysz <arek1nowosielski@gmail.com>
2022-06-08 13:02:39 +0200

    Printowanie zmiennych na dwa sposoby

--- Zmienne1.py
+++ Zmienne1.py
@@ -1,12 +1,3 @@
-<<<<<<< current
-zmienna1 = "poznajemy"
-zmienna2 = "Pythona"
-zmienna3 = "Dzisiaj"
-zmienna4 = "wiele"
-
-print(zmienna3 +" "+ zmienna1 + " możliwości " + zmienna2+", których jest "+zmienna4+".")
-print(f"{zmienna3} {zmienna1} możliwości {zmienna2}, których jest {zmienna4}.")
-=======
 #Printowanie zmiennych na dwa sposoby
 zmienna1 = "poznajemy"
 zmienna2 = "Pythona"
@@ -15,4 +6,3 @@ zmienna4 = "wiele"
 
 print(zmienna3 +" "+ zmienna1 + " możliwości " + zmienna2+", których jest "+zmienna4+".")
 print(f"{zmienna3} {zmienna1} możliwości {zmienna2}, których jest {zmienna4}.")
->>>>>>> before discard
