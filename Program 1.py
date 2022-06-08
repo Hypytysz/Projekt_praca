94b94055be8777e652ad11640cf67539f868a8c9
Hypytysz <arek1nowosielski@gmail.com>
2022-06-08 12:59:11 +0200

    Printowanie zmiennych na dwa sposoby

--- Program 1.py
+++
@@ -1,8 +0,0 @@
-#Printowanie zmiennych na dwa sposoby
-zmienna1 = "poznajemy"
-zmienna2 = "Pythona"
-zmienna3 = "Dzisiaj"
-zmienna4 = "wiele"
-
-print(zmienna3 +" "+ zmienna1 + " możliwości " + zmienna2+", których jest "+zmienna4+".")
-print(f"{zmienna3} {zmienna1} możliwości {zmienna2}, których jest {zmienna4}.")
