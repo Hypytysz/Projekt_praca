a99e87f5edbb2f3e063f37fcb25b594045e5f4d2
Hypytysz <arek1nowosielski@gmail.com>
2022-06-14 10:21:15 +0200

    Dodawanie imion do list

---
+++ Typy sekwencyjne/Listy/Listy3.py
@@ -0,0 +1,13 @@
+# Dodawanie imion do list
+imiona = []
+imiona.append(input("Podaj pierwsze imie:"))
+imiona.append(input("Podaj drugie imie:"))
+imiona.append(input("Podaj trzecie imie:"))
+imiona.append(input("Podaj czwarte imie:"))
+imiona.append(input("Podaj piate imie:"))
+
+print(f"Cześć {imiona[0]}!")
+print(f"Cześć {imiona[1]}!")
+print(f"Cześć {imiona[2]}!")
+print(f"Cześć {imiona[3]}!")
+print(f"Cześć {imiona[4]}!")
