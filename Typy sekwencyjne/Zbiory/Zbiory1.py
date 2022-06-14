aa677403a1fb402fbcbd00bcf1b9df39354528a3
Hypytysz <arek1nowosielski@gmail.com>
2022-06-14 10:37:57 +0200

    Liczenie zawartości zbioru

---
+++ Typy sekwencyjne/Zbiory/Zbiory1.py
@@ -0,0 +1,9 @@
+# Liczenie zawartości zbioru
+zbior = set()
+zbior.add(input("Wprowadz 1 liczbe:"))
+zbior.add(input("Wprowadz 2 liczbe:"))
+zbior.add(input("Wprowadz 3 liczbe:"))
+zbior.add(input("Wprowadz 4 liczbe:"))
+zbior.add(input("Wprowadz 5 liczbe:"))
+
+print(f"Zbior posiada {len(zbior)} unikatowych liczb")
