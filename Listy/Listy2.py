e2d4bc39b896d9cab02a700d7e718ea2292ae3e1
Hypytysz <arek1nowosielski@gmail.com>
2022-06-14 09:25:14 +0200

    Program sortujący duplikaty z dwóch list

---
+++ Listy/Listy2.py
@@ -0,0 +1,25 @@
+#Program sortujący duplikaty z dwóch list
+import random
+lista = []
+lista2 = []
+obie = []
+x = 10
+y = 10
+while x > 0:
+    x -= 1
+    lista.append(int(random.randint(1, 50)))
+while y > 0:
+    y -= 1
+    lista2.append(int(random.randint(1, 50)))
+
+for i in lista:
+    for j in lista2:
+        if i==j:
+            obie.append(i)
+            break
+
+
+
+print(lista)
+print(lista2)
+print(obie)
