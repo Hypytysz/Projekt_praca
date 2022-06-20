54119a8225d5b4c3659d6e5a3aed2afa3c9850d9
Hypytysz <arek1nowosielski@gmail.com>
2022-06-20 14:32:15 +0200

    Sortowanie listy

---
+++ Pętle/Pętle11.py
@@ -0,0 +1,19 @@
+# Sortowanie listy
+x=2
+lista= []
+while x != 0:
+    x = (int(input(f"Podaj liczbe calkowita: ")))
+    if x != 0 and x not in lista:
+        lista.append(x)
+        for indeks, wartosc in enumerate(lista):
+            a = (indeks-1)
+            if x < lista[a]:
+                lista.remove(x)
+                lista.insert(a, x)
+                print(lista)
+                break
+    elif x == 0:
+        print(lista)
+        break
+    else:
+        print(f"Ta liczba juz jest na liscie!")
