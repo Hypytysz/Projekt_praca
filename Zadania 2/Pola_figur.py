90de26ef3c11686e24f6b1ef76a54b62c8f61177
Hypytysz <arek1nowosielski@gmail.com>
2022-07-25 20:02:56 +0200

    Uaktualnione pola figur

--- Zadania 2/Pola_figur.py
+++ Zadania 2/Pola_figur.py
@@ -1,24 +1,52 @@
-# Okrąg, kwadrat - bok, promień, średnica, obwód,  pole
 import math
-# class Figure:
 
 
+class Figure:
+    def __add__(self, other):
+        c = self.__class__()
+        c.area = c.area + other.area
+        return c
 
-class Circle:
+    def __lt__(self, other):
+        z = self.__class__()
+        a = z.area < other.area
+        return a
+
+    def __le__(self, other):
+        c = self.__class__()
+        a = c.area <= other.area
+        return a
+
+    def __eq__(self, other):
+        c = self.__class__()
+        a = c.area == other.area
+        return a
+
+    def __ne__(self, other):
+        c = self.__class__()
+        a = c.area != other.area
+        return a
+
+    def __gt__(self, other):
+        c = self.__class__()
+        a = c.area > other.area
+        return a
+
+    def __ge__(self, other):
+        c = self.__class__()
+        a = c.area >= other.area
+        return a
+
+
+class Circle(Figure):
     def __init__(self, radius=1):
         if radius != 1:
             self.radius = radius
         else:
             self.radius = 1
+
     def __str__(self):
         return f"Circle, radius: {self.__radius}, diameter: {self.diameter}, area:  {self.area}"
-
-    def __add__(self, other):
-        c=Circle()
-        c.area = self.area + other.area
-        return c
-
-
 
     @property
     def radius(self):
@@ -41,56 +69,93 @@ class Circle:
     @property
     def area(self):
         return math.pi * (self.radius**2)
+
     @area.setter
     def area(self, value):
         self.radius = (value / math.pi) ** (1 / 2)
 
 
-
-class Square:
+class Square(Figure):
     def __init__(self, side=1):
         if side != 1:
             self.side = side
         else:
             self.side = 1
+
     def __str__(self):
         return f"Square, side: {self.side}, circuit: {self.circuit}, area: {self.area}"
-
-    def __add__(self, other):
-        s=Square()
-        s.area = self.area + other.area
-        return s
 
     @property
     def circuit(self):
         return self.side * 4
+
     @circuit.setter
     def circuit(self, value):
         self.side = value / 4
+
     @property
     def area(self):
-        return self.side **2
+        return self.side**2
+
     @area.setter
     def area(self, value):
-        self.side = value ** (1/2)
+        self.side = value ** (1 / 2)
 
-s = Square(2)
-c = Circle(2)
+
+class Triangle(Figure):
+    def __init__(self, side=1):
+        if side != 1:
+            self.side = side
+        else:
+            self.side = 1
+
+    def __str__(self):
+        return (
+            f"Triangle, side: {self.side}, circuit: {self.circuit}, area: {self.area}"
+        )
+
+    @property
+    def circuit(self):
+        return self.side * 3
+
+    @circuit.setter
+    def circuit(self, value):
+        self.side = value / 3
+
+    @property
+    def area(self):
+        return ((self.side**2) * (3 ** (1 / 2))) / 4
+
+    @area.setter
+    def area(self, value):
+        self.side = math.sqrt((4 * value) * (3 ** (1 / 2))) / 4
+        # self.side = (value/s)/(s/(s+))
+
+
+t = Triangle(2)
+s = Square(3)
+c = Circle(3)
+
+
+print(s, t)
+print(t + s)
+print(s, c)
+print(s + c)
+print(c + s)
 
 print(s, c)
-print(s+c)
-print(c+s)
-
+print(c == s)
+print(c >= s)
+print(c <= s)
+print(c != s)
+print(c > s)
+print(c < s)
 
 s = Square(2)
 print(s)
 s.circuit = 100
 print(s.side, s.circuit, s.area)
 print(s)
-
-
-
-
 
 
 c = Circle()
@@ -103,5 +168,5 @@ print(c.radius, c.diameter, c.area)
 a = Circle(4)
 b = Circle(3)
 
-print(a+b)
-print(c+s)
+print(a + b)
+print(c + s)
